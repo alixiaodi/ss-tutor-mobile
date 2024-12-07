6219493e4bc2c269d8ad847b8070736af92ff51d
lcs <1697383273@qq.com>
2024-12-07 20:12:40 +0800

    up

--- config/environment.js
+++ config/environment.js
@@ -2,10 +2,10 @@ const environment = {
 	// 开发环境配置
 	development: {
 		// 本地部署的后端
-		baseURL: 'http://localhost:6633',
+		// baseURL: 'http://localhost:6633',
 		
 		// 直接使用线上后端
-		// baseURL: 'https://tutorapi.lics.asia'
+		baseURL: 'https://tutorapi.lics.asia'
 	},
 	// 生产环境配置
 	production: {
